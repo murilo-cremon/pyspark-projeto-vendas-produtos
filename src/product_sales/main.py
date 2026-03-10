bd53eb9cfc4a0cdb7df6fd5d99db974ce2e9dc44
Murilo <murilo.ccoutinho@gmail.com>
2026-03-10 16:25:16 -0300

    Adding customer dimension

--- src/product_sales/main.py
+++ src/product_sales/main.py
@@ -2,12 +2,12 @@ from config.config import *
 from utils.spark_session import create_spark_session
 from ingestion.product_sales_ingestion import ingestion_file_raw_zone
 from processing.bronze_to_silver_product_sales import save_parquet_csv_file_to_trusted_zone
-from processing.product_dimension import save_parquet_csv_file_to_product_dimension
+from processing.customer_dimension import save_parquet_csv_file_to_customer_dimension
 
 spark = create_spark_session(APP_NAME)
 # print(f'Funcionou: {spark}')
 
 #ingestion_file_raw_zone(spark, LANDING_ZONE_FILE, RAW_ZONE)
-save_parquet_csv_file_to_product_dimension(spark, TRUSTED_ZONE, REFINED_ZONE)
+save_parquet_csv_file_to_customer_dimension(spark, TRUSTED_ZONE, REFINED_ZONE)
 
 # TRUSTED_ZONE
--- src/product_sales/processing/customer_dimension.py
+++ src/product_sales/processing/customer_dimension.py
@@ -18,12 +18,12 @@ def select_fields(dataframe):
     df = dataframe
 
     column_list = [
-        'id_produto',
-        'produto',
-        'categoria',
-        'subcategoria',
-        'fabricante',
-        'custo'
+        'id_cliente',
+        'nome',
+        'email',
+        'dt_cadastro',
+        'tipo_estado_civil',
+        'genero'
     ]
 
     df = (
@@ -39,33 +39,33 @@ def generate_surrogate_key(dataframe):
     df = dataframe
 
     column_list = [
-        'sk_produto',
-        'id_produto',
-        'produto',
-        'categoria',
-        'subcategoria',
-        'fabricante',
-        'custo'
+        'sk_cliente',
+        'id_cliente',
+        'nome',
+        'email',
+        'dt_cadastro',
+        'tipo_estado_civil',
+        'genero'
     ]
 
-    window = Window.orderBy('id_produto')
+    window = Window.orderBy('id_cliente')
 
     df = (
         df
-        .withColumn('sk_produto', row_number().over(window))
+        .withColumn('sk_cliente', row_number().over(window))
         .select(column_list)
     )
 
     return df
 
 
-def save_parquet_csv_file_to_product_dimension(spark, trusted_path, refined_path):
+def save_parquet_csv_file_to_customer_dimension(spark, trusted_path, refined_path):
     df = read_trusted_file_parquet(spark, trusted_path)
     df = select_fields(df)
     df = generate_surrogate_key(df)
 
-    csv_file_name = refined_path + '/product-dimension-csv'
-    parquet_file_name = refined_path + '/product-dimension-parquet'
+    csv_file_name = refined_path + '/customer-dimension-csv'
+    parquet_file_name = refined_path + '/customer-dimension-parquet'
 
     df.write \
         .option('header', 'true') \
